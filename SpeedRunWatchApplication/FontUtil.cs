89d3a74a223f5ca4e4f712a724dbc54214b44584
pathomchokej <86038423+pathomchokej@users.noreply.github.com>
2021-11-07 14:25:58 +0700

    Implement Main UI

---
+++ SpeedRunWatchApplication/FontUtil.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpeedRunWatchApplication
+{
+    class FontUtil
+    {
+        private float GetFontSize(Graphics gr, string text, Font font, int areaWidth, int areaHeight, int margin, float min_size, float max_size)
+        {
+            // Only bother if there's text.
+            if (text.Length == 0 || gr == null) return min_size;
+
+            // See how much room we have, allowing a bit
+            // for the Label's internal margin.
+            int wid = areaWidth - margin;
+            int hgt = areaHeight - margin;
+
+            // Make a Graphics object to measure the text.
+            using (gr)
+            {
+                while (max_size - min_size > 0.1f)
+                {
+                    float pt = (min_size + max_size) / 2f;
+                    using (Font test_font = new Font(font.FontFamily, pt))
+                    {
+                        // See if this font is too big.
+                        SizeF text_size = gr.MeasureString(text, test_font);
+                        if ((text_size.Width > wid) || (text_size.Height > hgt))
+                            max_size = pt;
+                        else
+                            min_size = pt;
+                    }
+                }
+                return min_size;
+            }
+        }
+    }
+}
---
+++ SpeedRunWatchApplication/FormMain.Designer.cs
@@ -0,0 +1,105 @@
+﻿
+namespace SpeedRunWatchApplication
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelDisplay = new System.Windows.Forms.Panel();
+            this.buttonControl = new System.Windows.Forms.Button();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.buttonConfig = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panelDisplay
+            // 
+            this.panelDisplay.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panelDisplay.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panelDisplay.Location = new System.Drawing.Point(12, 12);
+            this.panelDisplay.Name = "panelDisplay";
+            this.panelDisplay.Size = new System.Drawing.Size(444, 213);
+            this.panelDisplay.TabIndex = 0;
+            // 
+            // buttonControl
+            // 
+            this.buttonControl.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonControl.Location = new System.Drawing.Point(462, 12);
+            this.buttonControl.Name = "buttonControl";
+            this.buttonControl.Size = new System.Drawing.Size(100, 45);
+            this.buttonControl.TabIndex = 0;
+            this.buttonControl.Text = "Start";
+            this.buttonControl.UseVisualStyleBackColor = true;
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonReset.Location = new System.Drawing.Point(462, 63);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(100, 45);
+            this.buttonReset.TabIndex = 1;
+            this.buttonReset.Text = "Reset";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            // 
+            // buttonConfig
+            // 
+            this.buttonConfig.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonConfig.Location = new System.Drawing.Point(462, 114);
+            this.buttonConfig.Name = "buttonConfig";
+            this.buttonConfig.Size = new System.Drawing.Size(100, 45);
+            this.buttonConfig.TabIndex = 2;
+            this.buttonConfig.Text = "Config";
+            this.buttonConfig.UseVisualStyleBackColor = true;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(574, 229);
+            this.Controls.Add(this.buttonConfig);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.buttonControl);
+            this.Controls.Add(this.panelDisplay);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.MinimumSize = new System.Drawing.Size(300, 150);
+            this.Name = "FormMain";
+            this.Text = "Speed Run Watch";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelDisplay;
+        private System.Windows.Forms.Button buttonControl;
+        private System.Windows.Forms.Button buttonReset;
+        private System.Windows.Forms.Button buttonConfig;
+    }
+}
+
